ea404726936168cc08ff2789868c56a8349573c6
Luke Malpass <contact@angelsix.com>
2018-02-15 10:03:33 +0000

    Initial WebRequests class for comms between a HTTP server

---
+++ Source/Dna.Framework/Web/Constants/KnownContentSerializersExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace Dna
+{
+    /// <summary>
+    /// Extension methods for <see cref="KnownContentSerializers"/>
+    /// </summary>
+    public static class KnownContentSerializersExtensions
+    {
+        /// <summary>
+        /// Takes a known serializer type and returns the Mime type associated with it
+        /// </summary>
+        /// <param name="serializer">The serializer</param>
+        /// <returns></returns>
+        public static string ToMimeString(this KnownContentSerializers serializer)
+        {
+            // Switch on the serializer
+            switch (serializer)
+            {
+                // Json
+                case KnownContentSerializers.Json:
+                    return "application/json";
+
+                // XML
+                case KnownContentSerializers.Xml:
+                    return "application/xml";
+
+                // Unknown
+                default:
+                    return "application/octet-stream";
+            }
+        }
+    }
+}
