00c80fe0dda4e7f0c4a1d651378334d7bb45c312
Luke Malpass <contact@angelsix.com>
2020-05-13 08:08:43 +0100

    Safe null checking for helpers, min log level debug

--- Source/Dna.Framework/Framework/Framework.cs
+++ Source/Dna.Framework/Framework/Framework.cs
@@ -38,7 +38,7 @@ namespace Dna
         /// <summary>
         /// The dependency injection service provider
         /// </summary>
-        public static IServiceProvider Provider => Construction.Provider;
+        public static IServiceProvider Provider => Construction?.Provider;
 
         #endregion
 
